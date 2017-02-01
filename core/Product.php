99cd5544940473db0c8329d9eff7cad55e86e3f6
copklient <copklient@mail.ru>
2017-02-02 00:46:09 +0400

    inicializacia

--- core/Product.php
+++ core/Product.php
@@ -1,46 +1,70 @@
 <?php 
 namespace core;
+
 class Product{
-	public $title;
-	public $price;
-	public $status;
-	public $description;
-	public $categories;
-	public $custom_parms;
+	public $title = '';
+	public $price = '';
+	public $status = '';
+	public $description = '';
+	public $categories = array();
+	public $custom_parms = '';
+	public $error = '';
+	private $width = '100px';
+	private $height = '100px';
 
-	function __construct($title, $price, $status, $description, $categories, $custom_parms){
-		$this->title = $title;
-		$this->price = $price;
-		$this->status = $status;
-		$this->description = $description;
-		$this->categories = $categories;
-		$this->custom_parms = $custom_parms;
-	}
+	function __construct($title, $price, $status, $description, $categories = array(), $custom_parms = '', $width = '', $height = ''){
+		if(!empty($title) && is_string($title) && !empty($price) && !empty($status) && is_string($status) && !empty($description) && is_string($description) && !empty($categories) && is_array($categories)){
+			$this->title = $title;
+			$this->price = $price;
+			$this->status = $status;
+			$this->description = $description;
+			$this->categories = $this->printTree($categories);
+			$this->custom_parms = $custom_parms;
 
-	public function get(){
-		echo 'nice';
+			$this->width = $width;
+			$this->height = $height;
+		}else{
+			$this->error = 'Product parms error';  
+		}
 	}
 
 	public function getProduct(){
-		echo 'Title: '.$this->title;
-		echo '<br>Status: '.$this->status;
-		echo '<br>Price: '.$this->price;
-		echo '<br>Information: '.$this->description;
-		echo '<br>Tags: '.$this->categories;
+		if(empty($this->error)){
+			echo '<div style="display: inline-block;width='.$this->width.';height='.$this->height.'">';
+			echo 'Title: '.$this->title;
+			echo '<br>Status: '.$this->status;
+			echo '<br>Price: '.$this->price;
+			echo '<br>Information: '.$this->description;
+			echo '<br>Tags: '.$this->categories;
+			if(!empty($this->custom_parms))
+			echo '<br>Custom Parms: '.$this->custom_parms;
+			echo '</div>';
+		}
+		else{
+			echo $this->error;
+		}
 	}
 
 	function printTree($tree) {
 	    if(!is_null($tree) && count($tree) > 0) {
-	        echo '<ul>';
+	    	$a = 0;
+	        // echo '<ul>';
 	        foreach($tree as $kn=>$node) {
-	            echo '<li>'.$node['item']['name'];
-	            echo '<pre>';
-	            // var_dump($node['children']);echo count($node['children']);
-	            var_dump($node['children'][''.count($node['children']).'']);
-	            echo '<ul><li>'.$node['children'][count($node['children'])]['item']['name'].'</li></ul>';
-	            echo '</li>';
+	        	$data .= $node['item']['name'].', ';
+	            // echo '<li>'.$node['item']['name'];
+	            if(is_array($node['children']))
+	          		for($a = 0; $a <= count($node['children']); $a++){
+	          			$data .= $node['children'][$a]['item']['name'].', ';
+	          		 // echo '<ul><li>'.$node['children'][$a]['item']['name'].'</li></ul>';
+	          		  }
+	          	/*if(!empty($node['children'][0]['children']))
+	          		for($a = 0; $a <= count($node['children'][$a]['children']); $a++){ echo '<ul><li>'.$node['children'][$a]['children'][$a]['item']['name'].'</li></ul>'; } */ 
+	            // echo '</li>';
+	            $a++;
+	       		$data = trim($data,', , ');
+	            return $data;
 	        }
-	        echo '</ul>';
+	        // echo '</ul>';
 	    }
 	}
 }
--- index.php
+++ index.php
@@ -1,19 +1,22 @@
 <?php 
 
-// require_once 'core/autoload.php';
 require 'vendor/autoload.php';
 
 
+
+$data = array(
+    array(
+    	'item'=>array('id'=>'3','name'=>'third'),
+    	 'children'=>array(array('item'=>array('name'=>'first', 'id'=>1), 'children'=>array(array('item'=>array('name'=>'test','id'=>7)))),
+    	 array('item'=>array('name'=>'second', 'id'=>2)))),
+   // array('item'=>array('id'=>'5','name'=>'fifth'),
+     //'children'=>array(array('item'=>array('name'=>'fourth', 'id'=>4))))
+);
 // use core\Product;
-$product = new core\Product('meqena', '500$', 'avtive', 'lav vichakuma bayc paytaca', 'ped', '');
+$product = new core\Product('meqena', '500$', 'avtive', 'lav vichakuma bayc paytaca', $data, '');
 
 $product->getProduct();
 
 
-$data = array(
-    array('item'=>array('id'=>'3','name'=>'third'), 'children'=>array(array('item'=>array('name'=>'first', 'id'=>1)),    array('item'=>array('name'=>'second', 'id'=>2)))),
-    array('item'=>array('id'=>'5','name'=>'fifth'), 'children'=>array(array('item'=>array('name'=>'fourth', 'id'=>4))))
-);
-$product->printTree($data);
-$aaa = new core\aaa();
 
+$product->printTree($data);
